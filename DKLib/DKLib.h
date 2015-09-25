fcd6b6632344cf583f4abf23162752760633324f
dai <dai@mashups.co>
2015-09-25 13:40:20 +0900

    Initial Commit

---
+++ DKLib/DKLib.h
@@ -0,0 +1,19 @@
+//
+//  DKLib.h
+//  DKLib
+//
+//  Created by dai on 9/25/15.
+//  Copyright © 2015 nevitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DKLib.
+FOUNDATION_EXPORT double DKLibVersionNumber;
+
+//! Project version string for DKLib.
+FOUNDATION_EXPORT const unsigned char DKLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKLib/PublicHeader.h>
+
+

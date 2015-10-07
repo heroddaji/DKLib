514c0f644526d65696754fec9ed07e99b65cad8e
dai <dai@mashups.co>
2015-10-07 20:08:59 +0900

    add background queue

--- DKLib/utility/DKQueue.swift
+++ DKLib/utility/DKQueue.swift
@@ -13,4 +13,8 @@ class DKQueue{
     class func main(block:()->()){
         dispatch_async(dispatch_get_main_queue(), block)
     }
+    
+    class func background(block:()->()){
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
+    }
 }
